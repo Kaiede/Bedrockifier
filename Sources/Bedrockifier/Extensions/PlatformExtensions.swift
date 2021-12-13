b2631eb86dace6a1a874e5e3dfb6da5e451c9816
Kaiede <user@biticus.net>
2021-12-13 13:41:11 -0800

    Typealias Mode

--- Sources/Bedrockifier/Extensions/PlatformExtensions.swift
+++ Sources/Bedrockifier/Extensions/PlatformExtensions.swift
@@ -9,8 +9,10 @@ import Foundation
 
 #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
 import Darwin
+typealias PlatformMode = __darwin_mode_t
 #elseif os(Linux)
 import Glibc
+typealias PlatformMode = __mode_t
 #endif
 
 enum PlatformError: Error {
@@ -31,7 +33,7 @@ func platformChown(path: String, uid: UInt32?, gid: UInt32?) throws {
 
 func platformChmod(path: String, permissions: UInt16) throws {
     try path.withCString({ cchars in
-        let result = chmod(cchars, permissions)
+        let result = chmod(cchars, PlatformMode(permissions))
         guard result == 0 else {
             throw PlatformError.Errno(error: errno)
         }
