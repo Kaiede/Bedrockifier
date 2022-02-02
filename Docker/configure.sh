eb5e601ef23d4d5e98d1e52cf8ae079e59fe5981
Kaiede <user@biticus.net>
2022-02-01 16:26:13 -0800

    Fix Typo

--- Docker/configure.sh
+++ Docker/configure.sh
@@ -11,7 +11,7 @@ if [ "$arch" == "arm64" ]; then
     swift_version="${swift_version}-ubuntu-21.04"
 fi
 
-set-env swift_base
+export swift_base
 export swift_version
 echo Using Swift Image: $swift_base
 echo Using Swift Version: $swift_version
