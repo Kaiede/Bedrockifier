753f7cbe7919345358cfaa9d99b0e81cfbf07759
Kaiede <user@biticus.net>
2022-02-01 16:41:31 -0800

    Stop rendering the Dockerfile (#26)
    * Stop rendering the Dockerfile
    
    * Always run ARM build
    
    * Require build
    
    * Always run builds on pull request
    
    * Attempt Variable Export
    
    * Echo Settings
    
    * Fix Typo
    
    * Github Config Fixes?

---
+++ Docker/configure.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#
+# Outputs configuration variables for Github Actions and Local Builds
+
+arch=$1
+
+swift_base="swift"
+swift_version="5.5.2"
+if [ "$arch" == "arm64" ]; then 
+    swift_base="swiftarm/${swift_base}"
+    swift_version="${swift_version}-ubuntu-21.04"
+fi
+
+GITHUB_ENV=${GITHUB_ENV:-}
+if [ ! -z ${GITHUB_ENV} ]; then
+    echo Exporting GitHub Variables
+    echo "swift_base=${swift_base}" >> $GITHUB_ENV
+    echo "swift_version=${swift_version}" >> $GITHUB_ENV
+else
+    echo Exporting Local Variables
+    export swift_base
+    export swift_version
+fi
+
+echo Using Swift Image: $swift_base
+echo Using Swift Version: $swift_version
