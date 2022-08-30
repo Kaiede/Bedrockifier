030f711fc248423e4d31b5134c32c1ffa1b44163
Kaiede <user@biticus.net>
2022-08-30 12:18:52 -0700

    Use Official Images (Focal) for amd64 and arm64

--- build-docker.sh
+++ build-docker.sh
@@ -18,12 +18,12 @@ if [ "$arch" == "aarch64" ]; then
     arch=arm64
 fi
 
-. Docker/configure.sh $arch
+#. Docker/configure.sh $arch
 
 dockerTag=$dockerRepo:${tag}-${arch}
 
 docker build . -f Docker/Dockerfile \
     -t $dockerTag \
     --build-arg arch=${arch} \
-    --build-arg swift_base=${swift_base} \
-    --build-arg swift_version=${swift_version}
+#    --build-arg swift_base=${swift_base} \
+#    --build-arg swift_version=${swift_version}
