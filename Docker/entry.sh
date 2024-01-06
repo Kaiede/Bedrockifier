b0f9bfa2ecbd38f854a5398352c57acd57dcf87f
Kaiede <user@biticus.net>
2024-01-06 11:53:22 -0800

    Ensure that the account we create will match /backups after restart

--- Docker/entry.sh
+++ Docker/entry.sh
@@ -7,7 +7,9 @@
 
 # Configure User for SSH
 uid=$(stat -c %u /backups)
+deluser bedrockifier
 [ -x /usr/sbin/useradd ] && useradd -m -u ${uid} bedrockifier -s /bin/sh || adduser --disabled-login --uid ${uid} bedrockifier --shell /bin/sh;
+export HOME=/backups
 
 # Execute
 /usr/local/bin/entrypoint-demoter --match /backups --debug --stdin-on-term stop /opt/bedrock/bedrockifierd
