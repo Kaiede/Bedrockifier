13e3a2e1495f0355b8211c05393eca4467b383ef
Kaiede <user@biticus.net>
2024-04-23 18:22:30 -0700

    Check the correct folder for permissions

--- Docker/entry.sh
+++ Docker/entry.sh
@@ -4,5 +4,11 @@
 #
 # Used by the docker container
 
+# Figure out which folder to match
+FOLDER_TO_MATCH=/backups
+if [ -d /data ]; then
+  FOLDER_TO_MATCH=/data
+fi
+
 # Execute
-/usr/local/bin/entrypoint-demoter --match /backups --debug --stdin-on-term stop /opt/bedrock/bedrockifierd
+/usr/local/bin/entrypoint-demoter --match $FOLDER_TO_MATCH --debug --stdin-on-term stop /opt/bedrock/bedrockifierd
