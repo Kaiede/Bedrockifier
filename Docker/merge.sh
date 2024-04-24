eca1c578160c4c576b18fd6a25a4be743a6a2fa2
Kaiede <user@biticus.net>
2024-04-23 18:14:48 -0700

    Make release tags (#86)
    * Make both latest and release tags at the same time
    
    * Actually Run the Task on BuildJet

--- Docker/merge.sh
+++ Docker/merge.sh
@@ -3,9 +3,10 @@
 # Merges amd64 and arm64 into a single tag
 
 tag=$1
+sourceTag=$2
 
 echo Running 'docker buildx imagetools create'
 docker buildx imagetools create \
     -t $tag \
-    $tag-amd64 \
-    $tag-arm64 \
+    $sourceTag-amd64 \
+    $sourceTag-arm64 \
