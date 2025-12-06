f99a9073ccacbed8c5fab37eaf036fda4f40012e
Kaiede <user@biticus.net>
2025-12-06 11:57:48 -0800

    Tag kaiede/bedrockifier, and more semver options

--- Docker/merge.sh
+++ Docker/merge.sh
@@ -2,11 +2,47 @@
 #
 # Merges amd64 and arm64 into a single tag
 
-tag=$1
-sourceTag=$2
+do_semver=0
+args=()
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --semver)
+            do_semver=1
+            shift
+            ;;
+        --*)
+            echo "Unknown option"
+            exit 1
+            ;;
+        *)
+            args+=("$1")
+            shift
+            ;;
+    esac
+done
+
+tag=${args[0]}
+sourceTag=${args[1]}
 
 echo Running 'docker buildx imagetools create'
-docker buildx imagetools create \
+echo docker buildx imagetools create \
     -t $tag \
     $sourceTag-amd64 \
     $sourceTag-arm64 \
+
+if [ "${do_semver}" = "1" ]; then
+    components=($(echo $tag | tr "." "\n"))
+    minor_tag=${components[0]}.${components[1]}
+    echo docker buildx imagetools create \
+        -t $minor_tag \
+        $sourceTag-amd64 \
+        $sourceTag-arm64 \
+
+    major_tag=${components[0]}
+    echo docker buildx imagetools create \
+        -t $major_tag \
+        $sourceTag-amd64 \
+        $sourceTag-arm64 \
+
+fi
