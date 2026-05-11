8df7802c83874cdee9f22da4e5f4d69fb676515f
Kaiede <user@biticus.net>
2026-05-11 22:38:16 +0000

    Point back to the correct key

--- Docker/install-dependencies.sh
+++ Docker/install-dependencies.sh
@@ -16,7 +16,7 @@ else
     chmod a+r /etc/apt/keyrings/docker.asc
 
     # Add the repository to Apt sources:
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
         tee /etc/apt/sources.list.d/docker.list > /dev/null
 
     # Docker packages
