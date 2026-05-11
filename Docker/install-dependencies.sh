a2dfefe41b7c55e7aa738a93b90f33bf5a0eca95
Kaiede <user@biticus.net>
2026-05-11 22:31:54 +0000

    See if we can fix sources file issue

--- Docker/install-dependencies.sh
+++ Docker/install-dependencies.sh
@@ -16,13 +16,13 @@ else
     chmod a+r /etc/apt/keyrings/docker.asc
 
     # Add the repository to Apt sources:
-    tee /etc/apt/sources.list.d/docker.sources <<EOF
-    Types: deb
-    URIs: https://download.docker.com/linux/ubuntu
-    Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
-    Components: stable
-    Architectures: $(dpkg --print-architecture)
-    Signed-By: /etc/apt/keyrings/docker.asc
+    tee /etc/apt/sources.list.d/docker.sources <<EOF sed 's/^        //'
+        Types: deb
+        URIs: https://download.docker.com/linux/ubuntu
+        Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+        Components: stable
+        Architectures: $(dpkg --print-architecture)
+        Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
     # Docker packages
