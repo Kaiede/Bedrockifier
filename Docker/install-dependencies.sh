6ff7073a8ef4ac39c9a467877dc9b0d661cdd213
Kaiede <user@biticus.net>
2026-05-11 22:32:55 +0000

    Actually send to sed

--- Docker/install-dependencies.sh
+++ Docker/install-dependencies.sh
@@ -16,7 +16,7 @@ else
     chmod a+r /etc/apt/keyrings/docker.asc
 
     # Add the repository to Apt sources:
-    tee /etc/apt/sources.list.d/docker.sources <<EOF sed 's/^        //'
+    tee /etc/apt/sources.list.d/docker.sources <<EOF | sed 's/^        //'
         Types: deb
         URIs: https://download.docker.com/linux/ubuntu
         Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
