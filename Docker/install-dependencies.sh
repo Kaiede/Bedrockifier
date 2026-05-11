e03900290574fe30e7d978c21e9b916c58fd5547
Kaiede <user@biticus.net>
2026-05-11 15:56:14 -0700

    Patch Security Vulnerabilities found by Docker Scout (#162)
    - Updates Docker to official package feed
    - Updates dependencies from itzg to latest, adds support for Renovation bot PRs
    - Adds a 'slim' sub-tag that doesn't include Docker. Save some space / attack surface versus including it.

---
+++ Docker/install-dependencies.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Make
+set -xeuo pipefail
+
+apt update
+
+if [ "$1" == "slim" ]; then
+    apt install -y curl
+else
+    apt install -y ca-certificates curl
+
+    # Docker Repo
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+        tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    # Docker packages
+    apt update
+    apt install -y docker-ce docker-ce-cli containerd.io
+fi
+
+apt clean
+rm -rf /var/lib/apt/lists/*
